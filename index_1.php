41b62ee3739c7ab4c24f934cde692995b100ac8e
Ana Verde <anaverde89@gmail.com>
2021-05-18 15:09:53 -0400

    Introduccion a PHP

---
+++ index_1.php
@@ -0,0 +1,15 @@
+<?php
+
+/**Comentario de
+ *varias lineas
+ */
+class Answer
+{
+    /**
+     * Comentario de varias
+     * lineas para
+     * palabras
+     */
+    protected $clients = []; // Mi comentario
+    protected $insurers = []; # Mi comentario
+}
