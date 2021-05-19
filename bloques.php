b29eb53c88c6abda631885539e7d1d847a8ac28d
Ana Verde <anaverde89@gmail.com>
2021-05-19 16:11:06 -0400

    Bloques de códigos sencillos en PHP

---
+++ bloques.php
@@ -0,0 +1,44 @@
+<?php
+
+if (condition) {
+    # code...
+} elseif (condition) {
+    # code...
+} else {
+    # code...
+}
+
+
+switch ($login) {
+    case true:
+        # code...
+        break;
+    case false:
+        # code...
+        break;
+    default:
+        # code...
+        break;
+}
+
+//consulta a usuarios
+foreach ($datos as $row) {
+    # code...
+}
+
+$a = 100;
+while ($a <= 10) {
+    # code...
+    $a++;
+}
+
+
+//se ejecuta al menos una vez
+do {
+    # code...
+} while ($a <= 10);
+
+
+for ($i = 0; $i < 10; $i++) {
+    # code...
+}
